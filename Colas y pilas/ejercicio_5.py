8c7ad81573e7bc3421090407acc545ffb870532b
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2025-03-07 16:42:38 +0000

    5 a la mitad

--- Colas y pilas/ejercicio_5.py
+++ Colas y pilas/ejercicio_5.py
@@ -1,1 +1,39 @@
-class 
+class QuickChat:
+    def __init__(self):
+        self.q = []
+
+    def enqueue(self, mensaje: str) -> None:
+        self.q.append(mensaje)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.q.pop(0)
+        return None
+
+    def first(self):
+        if not self.is_empty():
+            return self.q[0]
+        return None
+
+    def is_empty(self) -> bool:
+        return len(self.q) == 0
+
+    def __repr__(self):
+        return str(self.q)
+    
+    def eliminacion(self):
+        pass
+
+# Ejemplo de uso
+q = QuickChat()
+q.enqueue("Hola como estas")
+q.enqueue("¿Cómo estas?")
+q.enqueue("Adios")
+q.enqueue("Adios")
+q.enqueue("Ayuda")
+print(q)
+q.dequeue()
+print(q)
+
+
+
