7e2953de14d57e139a47061c5eeb5d39965e3150
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2025-04-02 16:37:08 +0000

    tristeza

---
+++ listas_enlazadas/ejercicio_3.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, elemento: int):
+        self.elemento = elemento
+        self.next = None
+
+
+    
+Node1 = Node(1)
+Node2 = Node(2)
+Node3 = Node(3)
+Node4 = Node(4)
+
+Node1.next = Node4
+Node2.next = Node3
+Node3.next = Node1
+Node4.next = Node2
+
+print(Node1.next.next.next.elemento)
+
